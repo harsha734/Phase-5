1916e025be35f6af9c56845378a9cfb89890eeec
harshs734 <harsha478478@gmail.com>
2022-03-28 15:46:14 +0530

    CI/CD Deployement for Spring Boot App

---
+++ MySpringboot-WebApp/src/main/java/com/MySpring/Controller/MainController.java
@@ -0,0 +1,13 @@
+package com.MySpring.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index.html";
+  }
+}
